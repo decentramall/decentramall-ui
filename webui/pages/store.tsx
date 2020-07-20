d9b2222b83d8d0222d72a2edad18613962365de9
Conrado Fonseca <conradob@live.com>
2020-07-20 22:12:46 +0100

    Add Material-UI

--- webui/pages/store.tsx
+++ webui/pages/store.tsx
@@ -1,3 +1,16 @@
-export const Store = (): JSX.Element => <div>Example of a store</div>
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import React from 'react'
 
-export default Store
+export const Store = (): JSX.Element => {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Store
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
