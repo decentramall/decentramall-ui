cdd1057ac6d7e13d49c543beb06f85744d0cee55
Conrado Fonseca <conradob@live.com>
2020-07-21 11:26:56 +0100

    Implement static stores

--- webui/pages/index.tsx
+++ webui/pages/index.tsx
@@ -1,17 +1,66 @@
-import Box from '@material-ui/core/Box'
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  storeContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, 212px)',
+    gridAutoRows: '139px',
+    gridAutoFlow: 'dense',
+    gridGap: '3px',
+    background: `linear-gradient(to right, ${theme.palette.common.black}, ${theme.palette.grey['A400']})`,
+  },
+  store: {
+    overflow: 'hidden',
+    display: 'grid',
+    '& img': {
+      gridColumn: '1 / -1',
+      gridRow: '1 / -1',
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      top: '10px',
+    },
+    gridColumn: 'span 2',
+  },
+  logo: {
+    display: 'grid',
+    objectFit: 'cover',
+    gridColumn: 'span 6',
+    alignSelf: 'center',
+    justifySelf: 'center',
+  },
+}))
+
+const range = (start: number, stop: number, step: number) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
 
 export const Home = (): JSX.Element => {
+  const classes = useStyles()
+
+  const topItems = range(1, 12, 1)
+  const bottomItems = range(13, 27, 1)
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Decentramall
-        </Typography>
-      </Box>
-    </Container>
+    <>
+      <div className={classes.storeContainer}>
+        {topItems.map((el, index) => {
+          return (
+            <div key={`store-${index}`} className={`${classes.store}`}>
+              <img src={`/stores/store-${el}.png`} />
+            </div>
+          )
+        })}
+        <div className={classes.logo}>
+          <img src="/logo.png" />
+        </div>
+        {bottomItems.map((el, index) => {
+          return (
+            <div key={`store-${index}`} className={`${classes.store}`}>
+              <img src={`/stores/store-${el}.png`} />
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
