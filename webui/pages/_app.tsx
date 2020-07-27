2a1858058ceb4bd8484d4ef172de92dbdcd0eaa4
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-27 10:14:11 +0100

    load rent and space at _app

--- webui/pages/_app.tsx
+++ webui/pages/_app.tsx
@@ -2,34 +2,51 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import React from 'react'
+import React, { useState } from 'react'
 import theme from '../src/theme'
 
+
+export interface IChainContext {
+    spaces: string[];
+    rents: string[];
+}
+export const ChainContext = React.createContext<IChainContext>({
+    spaces: [],
+    rents: []
+});
+
 export default function MyApp(props: AppProps) {
-  const { Component, pageProps } = props
+    const [spaces, setSpaces] = useState<string[]>([]);
+    const [rents, setRents] = useState<string[]>([]);
+    const { Component, pageProps } = props
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles) {
-      jssStyles.parentElement!.removeChild(jssStyles)
-    }
-  }, [])
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side')
+        if (jssStyles) {
+            jssStyles.parentElement!.removeChild(jssStyles)
+        }
+        // TODO: load spaces
+        // TODO: load rents
+        setSpaces(['ola', 'xau']);
+    }, [])
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            <Head>
+                <title>My page</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <ChainContext.Provider value={{ spaces, rents }}>
+                    <Component {...pageProps} />
+                </ChainContext.Provider>
+            </ThemeProvider>
+        </React.Fragment>
+    )
 }
