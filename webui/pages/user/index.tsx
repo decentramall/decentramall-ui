6fd43060d9dfaafe5ac05b11d56df50209be8fed
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-28 21:03:18 +0100

    load user space token

--- webui/pages/user/index.tsx
+++ webui/pages/user/index.tsx
@@ -25,34 +25,43 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
     const classes = useStyles()
     const [view, setView] = useState<string | undefined>();
-    const choices = [
-        {
-            title: 'Buy SPACE',
-            picture: 'images/navigate-icons/category.svg',
-        },
-        {
-            title: 'Rent SPACE',
-            picture: 'images/navigate-icons/others.svg',
-        },
-    ]
 
     const openView = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, newView: string) => {
         setView(newView);
         event.preventDefault();
     }
 
+    const choices = (user: { space: {
+        buyer: string;
+        price: string;
+        tokenId: string;
+    } | undefined, rent: string }) => {
+        return [
+            {
+                title: (user.space !== undefined ? 'See your' : 'Buy') + ' SPACE',
+                picture: 'images/navigate-icons/category.svg',
+                view: 'space',
+            },
+            {
+                title: 'Rent SPACE',
+                picture: 'images/navigate-icons/others.svg',
+                view: 'rent',
+            },
+        ];
+    }
+
     const navigateOptions = (chainContext: IChainContext) => {
         if (view === 'space') {
-            return <Space spaces={chainContext.spaces} />;
+            return <Space />;
         } else if (view === 'rent') {
             return <Rent />;
         } else {
             return <Container maxWidth="sm">
                 {/* TODO: align vertically */}
                 <Grid container justify="center" spacing={5} style={{ marginTop: '40%' }}>
-                    {choices.map((choice) => (
+                    {choices(chainContext.user).map((choice) => (
                         <Grid key={choice.title} item>
-                            <Paper className={classes.paper}>
+                            <Paper className={classes.paper} onClick={(e) => openView(e, choice.view)}>
                                 <img height="95" src={choice.picture} />
                                 <Typography variant="body1" component="p" gutterBottom>
                                     {choice.title}
