ead5211993b30559823bfd38013e76e66b0fdd8b
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-28 21:03:18 +0100

    load user space token

--- webui/pages/user/space.tsx
+++ webui/pages/user/space.tsx
@@ -2,18 +2,27 @@ import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import React from 'react'
+import { ChainContext, IChainContext } from '../_app'
 
 
-interface ISpaceProps {
-    spaces: any[];
-}
-export default function Space(props: ISpaceProps) {
+
+export default function Space() {
+
+    const renderContext = (chainContext: IChainContext) => {
+        if (chainContext.user.space !== undefined) {
+            return <p>{JSON.stringify(chainContext.user.space)}</p>
+        } else {
+            return chainContext.spaces.map((space) => <p>{space}</p>)
+        }
+    }
     return (
         <Container maxWidth="sm">
             <Typography variant="h4" component="h1" gutterBottom>
                 Space
             </Typography>
-            {props.spaces.map((space) => <p>{space}</p>)}
+            <ChainContext.Consumer>
+                {(chainContext) => renderContext(chainContext)}
+            </ChainContext.Consumer>
         </Container>
     )
 }
