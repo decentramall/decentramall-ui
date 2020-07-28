4cea4636d50c549a75857556d6ddb2c33fac0f00
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-28 23:54:23 +0100

    rent

--- webui/pages/user/rent.tsx
+++ webui/pages/user/rent.tsx
@@ -1,46 +1,69 @@
 import { createPow } from '@textile/powergate-client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CreateFilecoinStorageDeal } from 'slate-react-system';
-import { Button, Input } from '@material-ui/core';
+import { Button, Input, makeStyles, TextField, CircularProgress } from '@material-ui/core';
 
 
-interface IRentState {
-    cid: string;
-    rented: string;
-    picture?: any;
-}
-class Rent extends React.Component<{}, IRentState> {
-    PowerGate = null
+export default function Rent() {
+    const classes = useStyles();
+    let PowerGate = null
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            cid: '',
-            rented: ''
-        };
-    }
+    const [picture, setPicture] = useState();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [url, setUrl] = useState('');
+    
+    const [pictureDealInProgress, setPictureDealInProgress] = useState(false);
+    const [jsonDealInProgress, setJSONDealInProgress] = useState(false);
 
-    componentDidMount = () => {
-        this.PowerGate = createPow({ host: process.env.NEXT_PUBLIC_POWERGATE_URL })
-        this.PowerGate.setToken(process.env.NEXT_PUBLIC_FFS_TOKEN)
+
+    useEffect(() => {
+        PowerGate = createPow({ host: process.env.NEXT_PUBLIC_POWERGATE_URL })
+        PowerGate.setToken(process.env.NEXT_PUBLIC_FFS_TOKEN)
         // TODO: chack if user has rented space
-    }
+    });
 
-    handleSubmitNewRent = () => {
+    const handleSubmitNewRent = async () => {
         // TODO: verify fields
         // TODO: upload image first
         // TODO: wait for image cid, generated json and upload it
         // TODO: wait for json cid, and add it to the tokenURI
+        const pictureCid = await _pictureStorageDeal();
+        const jsonRent = {
+            title,
+            description,
+            category,
+            logo: pictureCid,
+            url,
+        }
+
+
+        console.log(jsonRent);
+
+        const { cid } = await PowerGate.ffs.stage(new Uint8Array(Buffer.from(JSON.stringify(jsonRent))));
+        const { jobId } = await PowerGate.ffs.pushStorageConfig(cid);
+        console.log(jobId);
+        setJSONDealInProgress(true);
+        const cancel = PowerGate.ffs.watchJobs((job) => {
+            console.log(job);
+            // the status 5 means: deal finished
+            PowerGate.ffs.get(job.cid).then(console.log);
+            if (job.status === 5) {
+                setJSONDealInProgress(false);
+                cancel();
+            }
+        }, jobId);
     }
 
-    _getId = async () => {
-        // console.log(await this.PowerGate.ffs.get(this.state.cid));
-        console.log(await this.PowerGate.ffs.showAll());
+    const _getId = async () => {
+        // console.log(await PowerGate.ffs.get(this.state.cid));
+        console.log(await PowerGate.ffs.showAll());
     }
 
-    _handleSubmit = async (data) => {
+    const _pictureStorageDeal = async () => {
         //const file = data.file.files[0];
-        const file = this.state.picture;
+        const file = picture;
         if (file === undefined) {
             return;
         }
@@ -58,31 +81,65 @@ class Rent extends React.Component<{}, IRentState> {
                 reader.readAsArrayBuffer(file);
             });
         await getByteArray();
-        const { cid } = await this.PowerGate.ffs.stage(buffer);
-        const { jobId } = await this.PowerGate.ffs.pushStorageConfig(cid);
+        const { cid } = await PowerGate.ffs.stage(buffer);
+        const { jobId } = await PowerGate.ffs.pushStorageConfig(cid);
         console.log(jobId);
-        const cancel = this.PowerGate.ffs.watchJobs((job) => {
+        setPictureDealInProgress(true);
+        const cancel = PowerGate.ffs.watchJobs((job) => {
             console.log(job);
             // the status 5 means: deal finished
-            this.PowerGate.ffs.get(job.cid).then(console.log);
-            this.setState({ cid: job.cid });
+            PowerGate.ffs.get(job.cid).then(console.log);
+            if (job.status === 5) {
+                setPictureDealInProgress(false);
+                cancel();
+            }
         }, jobId);
+        return cid;
     }
 
-    selectImage = (event: React.ChangeEvent<any>) => {
-        this.setState({ picture: event.target.files[0] });
+    const selectImage = (event: React.ChangeEvent<any>) => {
+        setPicture(event.target.files[0]);
     }
 
-    render() {
-        // TODO: if valid rent space get
-        return (
-            <>
-                <Input type="file" onChange={this.selectImage} />
-                {/* <CreateFilecoinStorageDeal onSubmit={this._handleSubmit} /> */}
-                <Button onClick={this._handleSubmit}>Submit</Button>
-                <Button onClick={this._getId}>Get</Button>
-            </>
-        )
+    const handleChangeInput = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        switch (event.target.name) {
+            case 'title':
+                setTitle(event.target.value);
+                break;
+            case 'description':
+                setDescription(event.target.value);
+                break;
+            case 'category':
+                setCategory(event.target.value);
+                break;
+            case 'url':
+                setUrl(event.target.value);
+                break;
+        }
     }
+
+    // TODO: if valid rent space get
+    return (
+        <>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField label="Title" name="title" value={title} onChange={handleChangeInput} />
+                <TextField label="Description" name="description" value={description} onChange={handleChangeInput} />
+                <TextField label="Category" name="category" value={category} onChange={handleChangeInput} />
+                <TextField label="URL" name="url" value={url} onChange={handleChangeInput} />
+                <Input type="file" onChange={selectImage} />
+            </form>
+            <br />
+            {/* <CreateFilecoinStorageDeal onSubmit={this._handleSubmit} /> */}
+            <Button onClick={handleSubmitNewRent}>Submit</Button>
+            {/* <Button onClick={_getId}>Get</Button> */}
+            {(pictureDealInProgress || jsonDealInProgress) && <CircularProgress />}
+        </>
+    )
 }
-export default Rent
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: theme.spacing(1),
+        width: '250px',
+    },
+}))
