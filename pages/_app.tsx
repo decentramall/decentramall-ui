de1d79c03ba7810a81f6055f04dfe48f5490102f
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-28 11:37:56 +0100

    draft load space tokens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import React, { useState } from 'react'
 import theme from '../src/theme'
+import { ethers } from "ethers";
+// TODO: to change
+import DecentramallTokenJSON from '../../smart-contracts/build/contracts/DecentramallToken.json';
+import { DecentramallTokenInstance } from '../../smart-contracts/types/truffle-contracts/index';
 
 
 export interface IChainContext {
@@ -26,9 +30,35 @@ export default function MyApp(props: AppProps) {
         if (jssStyles) {
             jssStyles.parentElement!.removeChild(jssStyles)
         }
-        // TODO: load spaces
-        // TODO: load rents
-        setSpaces(['ola', 'xau']);
+
+        const loadWeb3 = async () => {
+            // A Web3Provider wraps a standard Web3 provider, which is
+            // what Metamask injects as window.ethereum into each page
+            const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+
+            // The Metamask plugin also allows signing transactions to
+            // send ether and pay to change state within the blockchain.
+            // For this, we need the account signer...
+            const signer = provider.getSigner();
+
+            const { chainId } = await provider.getNetwork();
+            const tokenContract = new ethers.Contract(
+                DecentramallTokenJSON.networks[chainId].address,
+                DecentramallTokenJSON.abi,
+                provider,
+            ) as ethers.Contract & DecentramallTokenInstance;
+
+            // TODO: load spaces
+            // a much more efficient way, would be to load from events, for example, using TheGraph
+            const totalTokens = await tokenContract.totalSupply();
+            for (let t = 0; t < totalTokens.toNumber(); t += 1) {
+                console.log(await tokenContract.tokenByIndex(t));
+            }
+            // TODO: load rents
+            setSpaces(['ola', 'xau']);
+        }
+
+        loadWeb3();
     }, [])
 
     return (
