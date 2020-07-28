36d0fe9cf00400ef4f91de2685c8058dcc9c6334
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-28 11:37:56 +0100

    draft load space tokens

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -2,10 +2,9 @@ const Administration = artifacts.require("Administration");
 const DecentramallToken = artifacts.require("DecentramallToken");
 const EstateAgent = artifacts.require("EstateAgent");
 const RentalAgent = artifacts.require("RentalAgent");
-const agent = '0xfe54a4bf10e879fc18b0facc4c8c3c72f9e082c5'; //ONE OF YOUR LOCAL BLOCKCHAIN ADDRESS
 
 module.exports = function (deployer, network, accounts) {
-  if (network !== 'development' && network !== 'coverage') {
+  if (network !== 'test' && network !== 'coverage') {
     let admin = accounts[0];
     deployer.then(async () => {
       //Always deploy EstateAgent first
