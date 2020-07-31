3022bd09e52eb5810b386602b45d94caf6c0d4ff
Eason Chai Jun Wei <43127107+easonchai@users.noreply.github.com>
2020-07-31 19:02:25 +0800

    Cleaned up the buttons & view/buy SPACE page

--- webui/pages/user/space.tsx
+++ webui/pages/user/space.tsx
@@ -51,26 +51,57 @@ export default function Space() {
 
     const renderContext = (chainContext: IChainContext) => {
         if (chainContext.user.space !== undefined) {
-            return <p>{JSON.stringify(chainContext.user.space)}</p>
+            var data = JSON.parse(JSON.stringify(chainContext.user.space));
+            return <Box display="flex" flexDirection="column" margin="auto" justifyContent="center" alignItems="center">
+            <Typography component="div" gutterBottom style={{marginTop: '4rem', textAlign: 'center'}}>
+                <Box fontWeight="lighter" fontSize="2rem" marginBottom="3rem">
+                Below are the details of your space
+                </Box>
+                <Box display="flex" flexDirection="row" fontSize="1.5rem" marginBottom="2rem">
+                    <Box fontWeight="bold" marginRight="1rem">
+                    Buyer: 
+                    </Box>
+                    <Box fontWeight="regular">
+                    {chainContext.user.space.buyer}
+                    </Box>
+                </Box>
+                <Box display="flex" flexDirection="row" fontSize="1.5rem" marginBottom="2rem">
+                    <Box fontWeight="bold" marginRight="1rem">
+                    Price:
+                    </Box>
+                    <Box fontWeight="regular">
+                    {(parseFloat(chainContext.user.space.price)/10**18).toFixed(8)} ETH
+                    </Box>
+                </Box>
+                <Box display="flex" flexDirection="row" fontSize="1.5rem" marginBottom="2rem">
+                    <Box fontWeight="bold" marginRight="1rem">
+                    Token ID:
+                    </Box>
+                    <Box fontWeight="regular">
+                    {chainContext.user.space.tokenId}
+                    </Box>
+                </Box>
+            </Typography>
+        </Box>;
         } else {
-            return <>
-                <Typography variant="h4" gutterBottom>
+            return <Box display="flex" flexDirection="column" margin="auto" justifyContent="center" alignItems="center">
+                <Typography variant="h5" gutterBottom style={{marginTop: '4rem', textAlign: 'center'}}>
                     This space would cost you {ethers.utils.formatEther(BigNumber.from(nextPrice))} ETH
                 </Typography>
-                <Button variant="contained" color="primary" onClick={buySpace}>
+                <Button variant="contained" color="primary" onClick={buySpace}  style={{marginTop: '2rem', width: '10vw'}}>
                     Buy this SPACE
                 </Button>
-            </>;
+            </Box>;
         }
     }
     return (
-        <Container maxWidth="md">
-            <Typography variant="h4" gutterBottom>
-                Space
+        <Box display="flex" flexDirection="column" style={{width:'85%', margin: 'auto'}}>
+            <Typography variant="h4" gutterBottom style={{marginTop: '4rem', textAlign: 'center', fontWeight: 'bold'}}>
+                SPACE
             </Typography>
             <ChainContext.Consumer>
                 {(chainContext) => renderContext(chainContext)}
             </ChainContext.Consumer>
-        </Container>
+        </Box>
     )
 }
