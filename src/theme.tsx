a71680771d35f44676f35e9ae85c9b7accdd5043
Bernardo Vieira <bernardo5@live.com.pt>
2020-08-03 19:54:43 +0100

    format

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,37 +1,37 @@
-import red from '@material-ui/core/colors/red'
-import { createMuiTheme } from '@material-ui/core/styles'
+import red from '@material-ui/core/colors/red';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 // Create a theme instance.
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        // background: {
+        //   paper: '#424242',
+        //   default: '#303030',
+        // },
+        // text: {
+        //   primary: '#fff',
+        //   secondary: 'rgba(255, 255, 255, 0.7)',
+        //   disabled: 'rgba(255, 255, 255, 0.5)',
+        //   hint: 'rgba(255, 255, 255, 0.5)',
+        // },
+        // divider: 'rgba(255, 255, 255, 0.12)',
     },
-    secondary: {
-      main: '#19857b',
+    typography: {
+        // "fontFamily": `"Ubuntu", "Asap", "Roboto", "Helvetica", "Arial", sans-serif`,
+        fontSize: 14,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
     },
-    error: {
-      main: red.A400,
-    },
-    // background: {
-    //   paper: '#424242',
-    //   default: '#303030',
-    // },
-    // text: {
-    //   primary: '#fff',
-    //   secondary: 'rgba(255, 255, 255, 0.7)',
-    //   disabled: 'rgba(255, 255, 255, 0.5)',
-    //   hint: 'rgba(255, 255, 255, 0.5)',
-    // },
-    // divider: 'rgba(255, 255, 255, 0.12)',
-  },
-  typography: {
-    // "fontFamily": `"Ubuntu", "Asap", "Roboto", "Helvetica", "Arial", sans-serif`,
-    "fontSize": 14,
-    "fontWeightLight": 300,
-    "fontWeightRegular": 400,
-    "fontWeightMedium": 500,
-  }
-})
+});
 
-export default theme
+export default theme;
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Home } from '../../pages/index'
-import { render } from '../testUtils'
+import React from 'react';
+import { Home } from '../../pages/index';
+import { render } from '../testUtils';
 
 describe('Home page', () => {
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
-  })
-})
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Home />, {});
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
